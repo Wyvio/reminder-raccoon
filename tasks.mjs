c5aa600c1ea7660163072c0ab97a4b1e7f2567c1
J <20728826+Wyvio@users.noreply.github.com>
2022-05-02 16:20:45 -0700

    test button but fails lol

--- tasks.mjs
+++ tasks.mjs
@@ -2,9 +2,29 @@ import { TodoistApi } from "@doist/todoist-api-typescript";
 
 const api = new TodoistApi('b7d45e930e39b1c97e8e90b8b3f907a47878e8ef')
 
-api.getTasks({filter: '(today | tomorrow)'})
-    .then((tasks) => 
-    {
-        for (const task of tasks) { console.log(task.content); }
-    })
-    .catch((error) => console.log(error))
+const generateButton = document.getElementById("generateButton");
+
+function generate() {
+    const list1 = new Array();
+    const list2 = new Array();
+
+    api.getTasks({filter: '(overdue | today)'})
+        .then((tasks) => 
+        {
+            for (const task of tasks) {
+                list1.push(task.content);
+            }
+            console.log(list1);
+        })
+        .catch((error) => console.log(error))
+
+    // api.getTasks({filter: '(tomorrow | 3 days)'})
+    //     .then((tasks) => 
+    //     {
+    //         for (const task of tasks) {
+    //             list2.push(task.content);
+    //         }
+    //         console.log(list2);
+    //     })
+    //     .catch((error) => console.log(error))
+}
