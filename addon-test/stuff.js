7658c8df7fb76b51571eba24065796f3fd953d30
J <20728826+Wyvio@users.noreply.github.com>
2022-05-04 12:18:39 -0700

    button works

---
+++ addon-test/stuff.js
@@ -0,0 +1,12 @@
+document.addEventListener("click", function(e) {
+  // if (!e.target.classList.contains("change-text")) {
+  //   return;
+  // }
+
+  document.querySelector(".change-text").textContent = "CHANGED";
+
+  // browser.tabs.create({
+  //   url: "https://google.com"
+  // });
+
+});
