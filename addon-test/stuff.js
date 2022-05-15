2e7b9abe36674ecd02b4e8502a6798701d7f2d1f
J <20728826+Wyvio@users.noreply.github.com>
2022-05-15 13:10:15 -0700

    tried some more stuff but it's still not working

--- addon-test/stuff.js
+++ addon-test/stuff.js
@@ -1,9 +1,11 @@
+const generateTasks = require('stuff2.mjs');
+
 document.addEventListener("click", function(e) {
   // if (!e.target.classList.contains("change-text")) {
   //   return;
   // }
 
-  //document.querySelector(".change-text").textContent = "CHANGED";
+  document.querySelector(".change-text").textContent = generateTasks();
 
   // browser.tabs.create({
   //   url: "https://google.com"
